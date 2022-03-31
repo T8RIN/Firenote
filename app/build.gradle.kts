d4c21873cac1614d7178a06cfc436ebd6bb90e64
T8RIN <oolbp@bk.ru>
2022-03-31 18:09:59 +0300

    updated dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,9 +17,6 @@ android {
         targetSdk = 32
         versionCode = 8
         versionName = "1.1.3"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
     }
 
     buildTypes {
@@ -44,7 +41,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.2.0-alpha05"
+        kotlinCompilerExtensionVersion = "1.2.0-alpha06"
     }
     packagingOptions {
         resources {
@@ -58,7 +55,7 @@ dependencies {
     //Android Essentials
     implementation("androidx.core:core-ktx:1.7.0")
     implementation("androidx.appcompat:appcompat:1.4.1")
-    implementation("com.google.android.material:material:1.6.0-alpha03")
+    implementation("com.google.android.material:material:1.6.0-beta01")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
     implementation("androidx.window:window:1.0.0")
     implementation("androidx.navigation:navigation-fragment-ktx:2.4.1")
@@ -73,7 +70,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
 
     //Dagger - Hilt
-    implementation("com.google.dagger:hilt-android:2.38.1")
+    implementation("com.google.dagger:hilt-android:2.39.1")
     implementation("androidx.hilt:hilt-navigation-fragment:1.0.0")
     implementation("androidx.lifecycle:lifecycle-service:2.4.1")
     kapt("com.google.dagger:hilt-android-compiler:2.38.1")
@@ -86,39 +83,31 @@ dependencies {
 
     //Compose
     implementation("androidx.activity:activity-compose:1.4.0")
-    implementation("androidx.compose.ui:ui:1.2.0-alpha05")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.2.0-alpha05")
-    implementation("androidx.compose.material3:material3:1.0.0-alpha07")
-    implementation("androidx.compose.material:material:1.2.0-alpha05")
+    implementation("androidx.compose.ui:ui:1.2.0-alpha06")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.2.0-alpha06")
+    implementation("androidx.compose.material3:material3:1.0.0-alpha08")
+    implementation("androidx.compose.material:material:1.2.0-alpha06")
     implementation("androidx.compose.material:material-icons-core:1.1.1")
     implementation("androidx.compose.material:material-icons-extended:1.1.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.0-alpha04")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.0-alpha05")
     implementation("androidx.navigation:navigation-compose:2.5.0-alpha03")
     implementation("androidx.constraintlayout:constraintlayout-compose:1.0.0")
-    implementation("androidx.compose.foundation:foundation:1.2.0-alpha05")
+    implementation("androidx.compose.foundation:foundation:1.2.0-alpha06")
 
     //Accompanist
     implementation("com.google.accompanist:accompanist-systemuicontroller:0.24.2-alpha")
     implementation("com.google.accompanist:accompanist-flowlayout:0.24.2-alpha")
-
-    //Tests
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.2.0-alpha05")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.2.0-alpha05")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.2.0-alpha05")
 
     //Coil
     implementation("io.coil-kt:coil:2.0.0-rc01")
     implementation("io.coil-kt:coil-compose:2.0.0-rc01")
 
     //Firebase
-    implementation("com.google.firebase:firebase-auth-ktx:21.0.2")
+    implementation("com.google.firebase:firebase-auth-ktx:21.0.3")
     implementation("com.google.android.gms:play-services-auth:20.1.0")
     implementation("com.google.firebase:firebase-database-ktx:20.0.4")
     implementation("com.google.firebase:firebase-storage-ktx:20.0.1")
     implementation("com.google.firebase:firebase-crashlytics-ktx:18.2.9")
-    implementation("com.google.firebase:firebase-analytics-ktx:20.1.1")
+    implementation("com.google.firebase:firebase-analytics-ktx:20.1.2")
 
 }
